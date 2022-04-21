c2e7f16874a524111c2403d66d7a785115eefb80
Funny_Silkie <ban.grumble99rar@gmail.com>
2022-04-21 11:39:45 +0900

    fix dotnet version

--- Ab1Analyzer/Program.cs
+++ Ab1Analyzer/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Ab1Analyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
