5a3ad0407154da73641599d306364132c1c11a35
Funny_Silkie <ban.grumble99rar@gmail.com>
2022-04-21 12:04:01 +0900

    totyu

--- Ab1Analyzer/Program.cs
+++ Ab1Analyzer/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.IO;
 
 namespace Ab1Analyzer
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            foreach (string path in args)
+            {
+                Console.WriteLine(path);
+                if (!File.Exists(path))
+                {
+                    Console.WriteLine("指定されたパスのファイルが存在しません。");
+                }
+                try
+                {
+                    using BinaryReader reader = CreateBinaryReader(path);
 
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"エラーが発生しました：{e.GetType().FullName}{Environment.NewLine}{e.Message}");
+                    continue;
+                }
+            }
+        }
+
+        /// <summary>
+        /// <see cref="BinaryReader"/>の新しいインスタンスを生成します。
+        /// </summary>
+        /// <param name="path"><読み込むファイルのパス/param>
+        /// <returns>パスに基づく<see cref="BinaryReader"/>の新しいインスタンス</returns>
+        private static BinaryReader CreateBinaryReader(string path)
+        {
+            var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+            return new BinaryReader(stream);
         }
     }
 }
