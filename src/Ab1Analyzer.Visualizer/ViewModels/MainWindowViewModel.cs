07bc981fcd08fa0c951bb3c889fa073f48ba8553
Funny_Silkie <ban.grumble99rar@gmail.com>
2022-05-01 15:26:13 +0900

    implement right menu

--- src/Ab1Analyzer.Visualizer/ViewModels/MainWindowViewModel.cs
+++ src/Ab1Analyzer.Visualizer/ViewModels/MainWindowViewModel.cs
@@ -21,6 +21,13 @@ namespace Ab1Analyzer.Visualizer.ViewModels
         private Ab1Wrapper wrapper;
 
         /// <summary>
+        /// プレート名を取得します。
+        /// </summary>
+        public ReadOnlyReactiveProperty<string> ContainerName { get; }
+
+        private ReactiveProperty<string> _ContainerName = new();
+
+        /// <summary>
         /// 開いているファイルのパスを取得します。
         /// </summary>
         public ReadOnlyReactiveProperty<string> OpenedFilePath { get; }
@@ -38,18 +45,51 @@ namespace Ab1Analyzer.Visualizer.ViewModels
         public ReadOnlyReactiveProperty<PlotModel> AnalyzedDataPlotModel { get; }
 
         /// <summary>
+        /// 配列を取得します。
+        /// </summary>
+        public ReadOnlyReactiveProperty<string> Sequence { get; }
+
+        private ReactiveProperty<string> _Sequence = new();
+
+        /// <summary>
+        /// Aのグラフを見せるかどうかを表す値を取得または設定します。
+        /// </summary>
+        public ReactiveProperty<bool> ShowA { get; } = new(true, ReactivePropertyMode.Default ^ ReactivePropertyMode.RaiseLatestValueOnSubscribe);
+
+        /// <summary>
+        /// Tのグラフを見せるかどうかを表す値を取得または設定します。
+        /// </summary>
+        public ReactiveProperty<bool> ShowT { get; } = new(true, ReactivePropertyMode.Default ^ ReactivePropertyMode.RaiseLatestValueOnSubscribe);
+
+        /// <summary>
+        /// Gのグラフを見せるかどうかを表す値を取得または設定します。
+        /// </summary>
+        public ReactiveProperty<bool> ShowG { get; } = new(true, ReactivePropertyMode.Default ^ ReactivePropertyMode.RaiseLatestValueOnSubscribe);
+
+        /// <summary>
+        /// Cのグラフを見せるかどうかを表す値を取得または設定します。
+        /// </summary>
+        public ReactiveProperty<bool> ShowC { get; } = new(true, ReactivePropertyMode.Default ^ ReactivePropertyMode.RaiseLatestValueOnSubscribe);
+
+        /// <summary>
         /// <see cref="MainWindowViewModel"/>の新しいインスタンスを初期化します。
         /// </summary>
         public MainWindowViewModel()
         {
             OpenedFilePath = _OpenedFilePath.ToReadOnlyReactiveProperty();
+            ContainerName = _ContainerName.ToReadOnlyReactiveProperty();
             RawDataPlotModel = new ReactiveProperty<PlotModel>(new PlotModel()).ToReadOnlyReactiveProperty();
             AnalyzedDataPlotModel = new ReactiveProperty<PlotModel>(new PlotModel()).ToReadOnlyReactiveProperty();
+            Sequence = _Sequence.ToReadOnlyReactiveProperty();
 
             InitPlotModel(RawDataPlotModel.Value, "RawData");
             InitPlotModel(AnalyzedDataPlotModel.Value, "AnalyzedData");
 
             _OpenedFilePath.Subscribe(OnOpenedFilePathChanged);
+            ShowA.Subscribe(OnShowAChanged);
+            ShowT.Subscribe(OnShowTChanged);
+            ShowG.Subscribe(OnShowGChanged);
+            ShowC.Subscribe(OnShowCChanged);
         }
 
         /// <summary>
@@ -60,6 +100,8 @@ namespace Ab1Analyzer.Visualizer.ViewModels
         {
             if (value == null)
             {
+                _ContainerName.Value = null;
+                _Sequence.Value = null;
                 ClearGraph(RawDataPlotModel.Value);
                 ClearGraph(AnalyzedDataPlotModel.Value);
                 return;
@@ -70,12 +112,65 @@ namespace Ab1Analyzer.Visualizer.ViewModels
                 wrapper = new Ab1Wrapper(data);
                 UpdateGraph(RawDataPlotModel.Value, wrapper.RawData);
                 UpdateGraph(AnalyzedDataPlotModel.Value, wrapper.AnalyzedData);
+                _ContainerName.Value = wrapper.ContainerName;
+                _Sequence.Value = wrapper.Sequence;
             }
             catch (Exception e)
             {
-                ClearGraph(RawDataPlotModel.Value);
                 MessageBox.Show($"ファイルの読み込みに失敗しました\n{e.GetType().FullName}: {e.Message}");
+                _ContainerName.Value = null;
+                _Sequence.Value = null;
+                ClearGraph(RawDataPlotModel.Value);
+                ClearGraph(AnalyzedDataPlotModel.Value);
             }
+        }
+
+        /// <summary>
+        /// <see cref="ShowA"/>が変更されたときに実行されます。
+        /// </summary>
+        /// <param name="value">設定された値</param>
+        private void OnShowAChanged(bool value)
+        {
+            RawDataPlotModel.Value.Series[0].IsVisible = value;
+            AnalyzedDataPlotModel.Value.Series[0].IsVisible = value;
+            RawDataPlotModel.Value.InvalidatePlot(true);
+            AnalyzedDataPlotModel.Value.InvalidatePlot(true);
+        }
+
+        /// <summary>
+        /// <see cref="ShowT"/>が変更されたときに実行されます。
+        /// </summary>
+        /// <param name="value">設定された値</param>
+        private void OnShowTChanged(bool value)
+        {
+            RawDataPlotModel.Value.Series[1].IsVisible = value;
+            AnalyzedDataPlotModel.Value.Series[1].IsVisible = value;
+            RawDataPlotModel.Value.InvalidatePlot(true);
+            AnalyzedDataPlotModel.Value.InvalidatePlot(true);
+        }
+
+        /// <summary>
+        /// <see cref="ShowG"/>が変更されたときに実行されます。
+        /// </summary>
+        /// <param name="value">設定された値</param>
+        private void OnShowGChanged(bool value)
+        {
+            RawDataPlotModel.Value.Series[2].IsVisible = value;
+            AnalyzedDataPlotModel.Value.Series[2].IsVisible = value;
+            RawDataPlotModel.Value.InvalidatePlot(true);
+            AnalyzedDataPlotModel.Value.InvalidatePlot(true);
+        }
+
+        /// <summary>
+        /// <see cref="ShowC"/>が変更されたときに実行されます。
+        /// </summary>
+        /// <param name="value">設定された値</param>
+        private void OnShowCChanged(bool value)
+        {
+            RawDataPlotModel.Value.Series[3].IsVisible = value;
+            AnalyzedDataPlotModel.Value.Series[3].IsVisible = value;
+            RawDataPlotModel.Value.InvalidatePlot(true);
+            AnalyzedDataPlotModel.Value.InvalidatePlot(true);
         }
 
         /// <summary>
@@ -140,7 +235,7 @@ namespace Ab1Analyzer.Visualizer.ViewModels
         /// </summary>
         /// <param name="model">更新する<see cref="PlotModel"/>のインスタンス</param>
         /// <param name="sequence">表示するシーケンスデータ</param>
-        private void UpdateGraph(PlotModel model, SequenceData sequence)
+        private static void UpdateGraph(PlotModel model, SequenceData sequence)
         {
             short min = 0;
             short max = 0;
